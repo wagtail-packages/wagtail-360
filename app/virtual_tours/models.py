7f0e6e56587a38100af7038a8b7370a292ac676c
Nick Moreton <nickmoreton@me.com>
2022-09-18 21:34:10 +0100

    Initial frontend

--- app/virtual_tours/models.py
+++ app/virtual_tours/models.py
@@ -7,6 +7,8 @@ class TourPage(Tour):
     subpage_types = ["virtual_tours.PanoramaPage"]
     content_panels = Page.content_panels + [Tour.panels]
 
+    template = "wagtail_360/tour_page.html"
+
 
 class PanoramaPage(Panorama):
     parent_page_types = ["virtual_tours.TourPage"]
