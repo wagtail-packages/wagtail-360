d478bbbbe706b35af7b7dc3b14d7f2fa6a7d1048
Nick Moreton <nickmoreton@me.com>
2023-02-16 22:31:10 +0000

    Alter tests

--- test/settings.py
+++ test/settings.py
@@ -117,20 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
-USE_I18N = True
-
-# USE_L10N = True
-
-USE_TZ = True
-
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
