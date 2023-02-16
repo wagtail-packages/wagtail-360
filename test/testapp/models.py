164d143543386b8dc97a5939061082469e1d0e73
Nick Moreton <nickmoreton@me.com>
2023-02-16 18:00:53 +0000

    Make abstract classes simpler

--- test/testapp/models.py
+++ test/testapp/models.py
@@ -1,6 +1,6 @@
 from wagtail.models import Page
 
-from wagtail_360.abstract_models import Panorama, Tour
+from wagtail_360.abstract_models import AbstractPanorama, AbstractTour
 
 
 class HomePage(Page):
@@ -10,14 +10,20 @@ class HomePage(Page):
         return context
 
 
-class TourPage(Tour):
+class TourPage(AbstractTour, Page):
     subpage_types = ["test_testapp.PanoramaPage"]
-    content_panels = Page.content_panels + Tour.content_panels
+    content_panels = Page.content_panels + AbstractTour.panels
+
+    def get_context(self, request, *args, **kwargs):
+        context = super(AbstractTour, self).get_context(request, *args, **kwargs)
+        context["panoramas"] = self.get_panoramas()
+        context["api_key"] = self.get_maps_api_key()
+        return context
 
 
-class PanoramaPage(Panorama):
+class PanoramaPage(AbstractPanorama, Page):
     parent_page_types = ["test_testapp.TourPage"]
-    content_panels = Page.content_panels + Panorama.content_panels
+    content_panels = Page.content_panels + AbstractPanorama.panels
 
     def serve_preview(self, request, mode_name):
         parent = self.get_parent()
--- wagtail_360/utils.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-
-
-def google_maps_api_key():
-    if hasattr(settings, "GOOGLE_MAPS_API_KEY"):
-        return settings.GOOGLE_MAPS_API_KEY
-    return ""
